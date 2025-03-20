a216019d107eba3a99c83b8729a1afcc0c3a519f
AchourSimoud <achoursimoud@gmail.com>
2025-03-20 11:41:54 +0100

    first commit

---
+++ cucumber.js
@@ -0,0 +1,21 @@
+module.exports = {
+  default: {
+    require: [
+      "step-definitions/**/*.ts",   // Path to step definitions
+      "hooks/**/*.ts"               // Path to hooks
+    ],
+    format: [
+      "allure-cucumberjs",                   // Pour les rapports Allure
+      "progress",                           // Affiche la progression dans le terminal
+      "json:reports/cucumber-report.json",   // JSON report pour Allure
+      "html:reports/cucumber-report.html"    // HTML report pour la lecture humaine
+    ],
+    tags: process.env.TAGS || "",  
+    worldParameters: {
+      env1Url: "http://int.siteinfos.com/admin/login/?next=/admin/",
+      env2Url: "http://rec.siteinfos.com/admin/login/?next=/admin/"
+    },
+    requireModule: ["ts-node/register"],     // Activer TypeScript
+    timeout: 10000                           // Temps d'exécution max (10s)
+  },
+};
